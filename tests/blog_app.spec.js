03071fc4ee99b28e7c37211576f5b2617f33579d
AnahiVera <vera.anahi.93@gmail.com>
2025-10-23 18:11:59 -0300

    fixed 5.17 added 5.18

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,35 +1,59 @@
-import { test, describe, expect } from '@playwright/test';
+import { test, describe, beforeEach, expect } from '@playwright/test';
 
 
 describe('Blog app', () => {
-test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:5173')
+    beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173')
+    })
 
-  const locator = page.getByText('Blogs')
-  await expect(locator).toBeVisible()
-  await expect(page.getByText('Blog app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
-})
+    
+    test('front page can be opened', async ({ page }) => {
+        await page.goto('http://localhost:5173')
+
+        const locator = page.getByText('Blogs')
+        await expect(locator).toBeVisible()
+        await expect(page.getByText('Blog app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+    })
 
 
- test('user can log in', async ({ page }) => {
-    await page.goto('http://localhost:5173')
+    test('Log in form is shown', async ({ page }) => {
+        await page.getByRole('button', { name: 'Login' }).click()
+        await expect(page.getByRole('textbox').first()).toBeVisible()
+        await expect(page.getByRole('textbox').last()).toBeVisible()
+    })
 
-    //click en toogle Login form
-    await page.getByRole('button', { name: 'Login' }).click()
-    //llenar formulario-> usamos first and last para seleccionar los input por orden
-    await page.getByRole('textbox').first().fill('Anahi')
-    await page.getByRole('textbox').last().fill('1234')
 
-    // all method tambien para llenar formularios (para mas de 2 campos)
-/* const textboxes = await page.getByRole('textbox').all()
-    await textboxes[0].fill('Anahi')
-    await textboxes[1].fill('1234') */
+         
 
-    await page.getByRole('button', { name: 'login' }).click()
-  
-    await expect(page.getByText('Anahi logged in')).toBeVisible()
-  })
 
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+
+              //click en toogle Login form
+        await page.getByRole('button', { name: 'Login' }).click()
+        //llenar formulario-> usamos first and last para seleccionar los input por orden
+        await page.getByRole('textbox').first().fill('Anahi')
+        await page.getByRole('textbox').last().fill('1234')
+
+        // all method tambien para llenar formularios (para mas de 2 campos)
+        /* const textboxes = await page.getByRole('textbox').all()
+            await textboxes[0].fill('Anahi')
+            await textboxes[1].fill('1234') */
+
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('Anahi logged in')).toBeVisible()
+            
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByRole('button', { name: 'Login' }).click()
+            await page.getByRole('textbox').first().fill('wrong user')
+            await page.getByRole('textbox').last().fill('wrong password')
+            await page.getByRole('button', { name: 'login' }).click()
+            await expect(page.getByText('Wrong credentials')).toBeVisible()
+        })
+    })
 
 
 })
